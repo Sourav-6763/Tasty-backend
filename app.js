3607a16b7f441815bd523a94e14d8d35aefa85b6
Sourav <srvsarkar2023@gmail.com>
2025-08-04 11:43:01 +0530

    Resolve merge conflict xxxxxxxx

--- app.js
+++ app.js
@@ -45,7 +45,6 @@ app.get('/api/health', (req, res) => {
   res.status(200).json({ status: "ok", uptime: process.uptime() });
 });
 
-
 app.use('/api/recipe',recipeRouter);
 app.use('/api/auth', authRouter); 
 app.use('/api/user', userRouter); 
